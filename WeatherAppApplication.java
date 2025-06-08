f874cce87f1cd94cf8823d8f27159cdedbeca593
nogu10 <testyou0430@gmail.com>
2025-06-08 22:12:53 +0900

    Update WeatherAppApplication.java

--- WeatherAppApplication.java
+++ WeatherAppApplication.java
@@ -3,8 +3,24 @@ package com.example.weatherapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * エントリーポイントとなるクラスです。
+ * <p>
+ * Spring Boot アプリケーションとして {@code WeatherAppApplication} を起動します。
+ * </p>
+ */
 @SpringBootApplication
 public class WeatherAppApplication {
+
+    /**
+     * アプリケーションのメインメソッドです。
+     * <p>
+     * Spring Boot の {@link SpringApplication#run(Class, String...)} を使用して
+     * アプリケーションを起動します。
+     * </p>
+     *
+     * @param args コマンドライン引数
+     */
     public static void main(String[] args) {
         SpringApplication.run(WeatherAppApplication.class, args);
     }
